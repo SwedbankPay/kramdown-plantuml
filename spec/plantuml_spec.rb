8d9e86b40a3a4f80d8afc19c53259118945af21c
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-03-27 17:39:34 +0100

    Split test up in parts with context

--- spec/plantuml_spec.rb
+++ spec/plantuml_spec.rb
@@ -1,18 +1,30 @@
 require "swedbank/pay/jekyll/plantuml/plantuml_converter"
 
 describe Swedbank::Pay::Jekyll::Plantuml::Converter do
-    it "creates plantuml" do
-        converter = Swedbank::Pay::Jekyll::Plantuml::Converter.new
-        cwd = File.dirname(__FILE__)
-        plantuml_file = File.join(cwd, 'diagram.plantuml')
-        plantuml_content = File.read(plantuml_file)
+    converter = Swedbank::Pay::Jekyll::Plantuml::Converter.new
+    cwd = File.dirname(__FILE__)
+    plantuml_file = File.join(cwd, 'diagram.plantuml')
+    plantuml_content = File.read(plantuml_file)
+
+    context "generates a diagram" do
         converted_svg = converter.convert_plantuml_to_svg(plantuml_content).to_s
 
         puts converted_svg
 
-        expect(converted_svg).not_to include("No @startuml/@enduml found")
-        expect(converted_svg).to include("<ellipse")
-        expect(converted_svg).to include("<polygon")
-        expect(converted_svg).to include("<path")
+        it "that is not erroneous" do
+            expect(converted_svg).not_to include("No @startuml/@enduml found")
+        end
+
+        it "that contains an ellipse" do
+            expect(converted_svg).to include("<ellipse")
+        end
+
+        it "that contains a polygon" do
+            expect(converted_svg).to include("<polygon")
+        end
+
+        it "that contains a path" do
+            expect(converted_svg).to include("<path")
+        end
     end
 end
