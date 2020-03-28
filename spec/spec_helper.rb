fe6091dc789db5d1ca21ecdda37440b73075ac6d
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-03-29 00:51:19 +0100

    It should say unless, no?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,8 +44,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  config.filter_run_excluding :only_if => 'nojava' if ENV['NO_JAVA'] == 1
-  config.filter_run_excluding :only_if => 'java' if ENV['JAVA'] == 1
+  config.filter_run_excluding :only_if => 'nojava' unless ENV['NO_JAVA'] == 1
+  config.filter_run_excluding :only_if => 'java' unless ENV['JAVA'] == 1
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
