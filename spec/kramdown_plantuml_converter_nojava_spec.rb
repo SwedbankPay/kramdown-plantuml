234d5ebc6198432929e4d23f58840cc2fbf6cb90
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-03-29 00:09:22 +0100

    The start of a workflow to provoke failures in the case Java is not installed

---
+++ spec/kramdown_plantuml_converter_nojava_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+require "swedbank-pay-jekyll-plantuml/plantuml_converter"
+
+describe SwedbankPayJekyllPlantuml::Converter, :only_if => :nojava do
+    converter = SwedbankPayJekyllPlantuml::Converter.new
+
+    context "fails properly" do
+        it "if Java is not installed" do
+            converter.convert_plantuml_to_svg(plantuml_content)
+        end
+    end
+end
