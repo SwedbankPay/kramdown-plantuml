3ed426cb12553e7eda190c3ba8f2f90e99582a0e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-03-30 16:24:06 +0200

    Various adjustments

--- spec/kramdown_plantuml_converter_nojava_spec.rb
+++ spec/kramdown_plantuml_converter_nojava_spec.rb
@@ -1,13 +1,11 @@
 require "spec_helper"
 require "swedbank-pay-jekyll-plantuml/plantuml_converter"
 
-describe SwedbankPayJekyllPlantuml::Converter, :exclusively => :nojava do
-    converter = SwedbankPayJekyllPlantuml::Converter.new
-
+describe SwedbankPayJekyllPlantuml::Converter, :exclusively => :no_java do
     context "fails properly" do
         it "if Java is not installed" do
             expect {
-                converter.convert_plantuml_to_svg('plantuml')
+                converter = SwedbankPayJekyllPlantuml::Converter.new
             }.to raise_error(IOError, "Java not found")
         end
     end
--- spec/kramdown_plantuml_converter_noplantuml_spec.rb
+++ spec/kramdown_plantuml_converter_noplantuml_spec.rb
@@ -2,12 +2,10 @@ require "spec_helper"
 require "swedbank-pay-jekyll-plantuml/plantuml_converter"
 
 describe SwedbankPayJekyllPlantuml::Converter, :exclusively => :no_plantuml do
-    converter = SwedbankPayJekyllPlantuml::Converter.new
-
     context "fails properly" do
         it "if plantuml.jar is not present" do
             expect {
-                converter.convert_plantuml_to_svg('plantuml')
+                converter = SwedbankPayJekyllPlantuml::Converter.new
             }.to raise_error(IOError, "'plantuml.1.2020.5.jar' not found")
         end
     end
