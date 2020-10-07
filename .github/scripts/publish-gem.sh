4f072e3c38c74c4fb55a8387145672d79a84fdd8
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-10-07 23:03:37 +0200

    Use publish-gem.sh in publish-prod as well

--- .github/scripts/publish-gem.sh
+++ .github/scripts/publish-gem.sh
@@ -5,13 +5,15 @@ me=$(basename "$0")
 
 help_message="\
 Usage:
-  $me --token <token> --owner <owner> --gem <gem-path> [--verbose]
+  $me --gem <gem-path> --token <token> [--owner <owner>] [--verbose]
   $me --help
 Arguments:
-  -t, --token <token>       The GitHub token to use for publishing the gem
-                            to the GitHub Package Registry.
-  -o, --owner <owner>       The path to the working directory.
-  -g, --gem <gem-path>      The path to directory of the Gem file to test.
+  -g, --gem <gem-path>      The path of the Gem to publish.
+  -t, --token <token>       The access token to use for publishing the gem to
+                            the specified registry.
+  -o, --owner <owner>       The owner to use when publishing to the GitHub
+                            Package Registry. If empty, will publish to
+                            RubyGems.org.
   -h, --help                Displays this help screen.
   -v, --verbose             Increase verbosity. Useful for debugging."
 
@@ -37,20 +39,14 @@ parse_args() {
         fi
     done
 
-    if [[ -z "$token" ]]; then
-        echo "Missing required argument: --token <token>."
-        echo "$help_message"
-        return 1
-    fi
-
-    if [[ -z "$owner" ]]; then
-        echo "Missing required argument: --owner <owner>."
-        echo "$help_message"
-        return 1
-    fi
-
     if [[ -z "$gem" ]]; then
         echo "Missing required argument: --gem <gem>."
+        echo "$help_message"
+        return 1
+    fi
+
+    if [[ -z "$token" ]]; then
+        echo "Missing required argument: --token <token>."
         echo "$help_message"
         return 1
     fi
@@ -68,17 +64,26 @@ publish_gem() {
     gem_home="$HOME/.gem"
     credentials_file="$gem_home/credentials"
 
+    if [[ -n "$owner" ]]; then
+        auth_header="github: Bearer"
+        host="https://rubygems.pkg.github.com/$owner"
+    else
+        auth_header="rubygems_api_key:"
+    fi
+
     mkdir -p "$gem_home"
     touch "$credentials_file"
     chmod 0600 "$credentials_file"
-    printf -- "---\n:github: Bearer %s\n" "$token" > "$credentials_file"
+    printf -- "---\n:%s %s\n" "$auth_header" "$token" > "$credentials_file"
 
     set -e
-    gem push \
-        --KEY github \
-        --host "https://rubygems.pkg.github.com/$owner" \
-        "$gem" \
-        || echo "push failed ($?) probably due to version '$gem' already existing in GPR."
+
+    if [[ -n "$host" ]]; then
+        gem push --KEY github --host "$host" "$gem" \
+            || echo "push failed ($?) probably due to version '$gem' already existing in GPR."
+    else
+        gem push "$gem"
+    fi
 }
 
 main() {
