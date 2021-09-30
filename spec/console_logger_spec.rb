37c5953ce2b0b42c3c053455dc45c001dc82cdfe
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-09-30 15:06:37 +0200

    DX-914: Add specs for ConsoleLogger

---
+++ spec/console_logger_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rspec/its'
+require 'kramdown-plantuml/logger'
+require 'kramdown-plantuml/console_logger'
+
+ConsoleLogger = ::Kramdown::PlantUml::ConsoleLogger
+
+describe ConsoleLogger do
+  describe '#new' do
+    [:debug, :info, :warn, :error].each do |level|
+      context "level: #{level}" do
+        subject { ConsoleLogger.new(level) }
+        it { is_expected.to respond_to(level) }
+
+        describe "\##{level}" do
+          it "receives \##{level}('test')" do
+            expect_any_instance_of(Kramdown::PlantUml::ConsoleLogger).to receive(level).with('test')
+            subject.public_send(level, 'test')
+          end
+        end
+      end
+    end
+  end
+end
